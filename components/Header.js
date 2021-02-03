9641a5145f5b266eb450b6b4fd76db3b27a2876f
crypt096 <acvjetan@gmail.com>
2021-02-03 18:34:19 +0100

    Add styled jsx

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,19 @@
 import headerStyles from '../styles/Header.module.css';
 
 const Header = () => {
+  const x = 2;
   return (
     <div>
-      <h1>
+      <h1 className="title">
         <span>WebDev</span> News
       </h1>
+      <style jsx>
+        {`
+          .title {
+            color: ${x > 3 ? 'red' : 'blue'};
+          }
+        `}
+      </style>
     </div>
   );
 }
