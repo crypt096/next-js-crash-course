c70236571f3f313f1655212064b6507bd6507179
crypt096 <acvjetan@gmail.com>
2021-02-03 18:43:51 +0100

    Add getStatic props and data fetching.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ articles }) {
+  console.log(articles)
   return (
     <div>
       <Head>
         <title>WebDev Newz</title>
         <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>Welcome to next</h1>
+      {articles.map(article => <h3>{article.title}</h3>)}
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?limit=6`);
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
